dc37ba5a960d9337bfec428caae87a59dbc58058
paoloemilioserra <paolo.serra@autodesk.com>
2024-05-26 11:52:00 +0200

    Refactor

--- reflex_i18next/reflex_i18next.py
+++ reflex_i18next/reflex_i18next.py
@@ -25,73 +25,21 @@ def key_recursion(t: dict[str, Any], prefix: str = '') -> dict[str, Any]:
     return ret
 
 
-class DynamicDict(dict):
-
-    def __init__(self):
-        super().__init__()
-
-    def __contains__(self, item: str) -> bool:
-        res = True
-        if isinstance(item, str):
-            if item in self.keys():
-                return True
-            t = self
-            for k in item.split('.'):
-                if k in t.keys():
-                    t = t.get(k, {})
-                else:
-                    res = False
-            return res
-        return False
-
-    def __getitem__(self, item: str) -> Any:
-        if isinstance(item, str):
-            if item in self.keys():
-                return self.get(item)
-            t = self
-            for k in item.split('.'):
-                if k in t.keys():
-                    t = t.get(k, {})
-                else:
-                    raise KeyError(f'{item=} not found')
-            self[item] = t
-            return t
-        raise ValueError(f'{item=} must be a string')
-
-    def __setitem__(self, key: str, value: Any) -> None:
-        if isinstance(key, str):
-            keys = key.split('.')
-            n = len(keys) - 2
-            t = self
-            for i, k in enumerate(keys):
-                t = t.setdefault(k, {})
-                if i == n:
-                    t.update({k: value})
-                    return
-        raise ValueError(f'{key=} must be a string')
-
-    def update(self, __m, **kwargs):
-        super().update(__m, **kwargs)
-
-
 class State(rx.State):
     """The app state."""
 
     language: str = 'en'
     path: str = 'assets/locales'
     strings: dict[str, str] = {}
+    LANGUAGES: list[str] = [f.name.lower() for f in pathlib.Path(path).iterdir() if f.is_dir()]
 
-    def click_en(self):
-        self.language = 'en'
-        self.update_strings()
-
-    def click_it(self):
-        self.language = 'it'
-        self.update_strings()
+    def click_language(self, lng: str):
+        if lng.lower() in self.LANGUAGES:
+            self.language = lng
+            self.update_strings()
 
     def update_strings(self):
-        dd = DynamicDict()
-        dd.update(json.loads((pathlib.Path(self.path) / self.language / 'translation.json').read_text(encoding='utf-8')))
+        dd = json.loads((pathlib.Path(self.path) / self.language / 'translation.json').read_text(encoding='utf-8'))
         dd = key_recursion(dd)
         self.strings = dd
 
@@ -104,32 +52,15 @@ def index() -> rx.Component:
         rx.text(State.strings['description.part2']),
         rx.button(
             'EN',
-            on_click=State.click_en
+            on_click=lambda: State.click_language('en')
         ),
         rx.button(
             'IT',
-            on_click=State.click_it
+            on_click=lambda: State.click_language('it')
         ),
-
+        on_mount=State.update_strings
     )
 
 
 app = rx.App()
 app.add_page(index)
-
-
-def test():
-    dd = DynamicDict()
-    dd.update({
-  "hello": "This is the 'Hello World' message in English",
-  "description": {
-    "part1": "This is the first part",
-    "part2": "This is the second part"
-  }
-})
-    assert dd['description.part1'] == 'This is the first part'
-
-    dd['2.2'] = 'd'
-    assert dd['2.2'] == 'd'
-
-
